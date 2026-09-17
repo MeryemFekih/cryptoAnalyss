e7540ce4013aa68508ac8c15972976e5fa541ee0
Meryem Fekih <meriamfekihsep@gmail.com>
2026-09-17 14:25:43 +0200

    code modification

--- screens/MyHome.jsx
+++ screens/MyHome.jsx
@@ -44,7 +44,7 @@ export function MyHome(){
   return (
     <ImageBackground style={{flex:1}} source={ImageBack} imageStyle={{opacity:0.5}}>
       <View style={{marginBottom:60}}/>
-      <TextInput style={{borderRadius:20,p}} placeholder='Entre votre crypto' i/>
+      <TextInput style={{borderRadius:20}} placeholder='Entre votre crypto' i/>
       
       <FlatList
        data={dataCryptos}
